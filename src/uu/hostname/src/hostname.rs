dc050526d190d5c04bc5100af47bb74bf70bad5c
Sylvestre Ledru <sylvestre@debian.org>
2024-03-26 23:02:08 +0100

    initial version of hostname

--- src/uu/hostname/src/hostname.rs
+++ src/uu/hostname/src/hostname.rs
@@ -3,19 +3,25 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
-use clap::crate_version;
-use clap::{Arg, Command};
-use std::process::{Command as SystemCommand, Stdio};
-use std::thread::sleep;
-use std::time::Duration;
-use uucore::{error::UResult, format_usage, help_about, help_usage};
+use std::ffi::OsString;
 
+use clap::crate_version;
+use clap::Command;
+
+use uucore::{error::UResult, format_usage, help_about, help_usage};
 const ABOUT: &str = help_about!("hostname.md");
 const USAGE: &str = help_usage!("hostname.md");
 
 #[uucore::main]
 pub fn uumain(args: impl uucore::Args) -> UResult<()> {
     let matches = uu_app().try_get_matches_from(args)?;
+
+    // Retrieve and display the hostname
+
+    let hostname = hostname::get().unwrap_or(OsString::from(""));
+    let hostname = hostname.to_string_lossy();
+    println!("{}", hostname);
+    // TODO implement the set using the same crate
     Ok(())
 }
 
