cde5990c0c8bc2224e602a8e5642c85fff7ab306
Anonymous-AAA <alenantonyme@gmail.com>
2025-07-20 18:27:08 +0530

    applied cargo fmt and fixed import

--- src/uu/domainname/src/net.rs
+++ src/uu/domainname/src/net.rs
@@ -1,9 +1,7 @@
 #[cfg(not(target_family = "windows"))]
 mod unix;
 
-
 #[cfg(not(target_family = "windows"))]
 pub(crate) use unix::*;
-
 
 pub(crate) struct LibraryGuard;
--- src/uu/domainname/src/print/unix.rs
+++ src/uu/domainname/src/print/unix.rs
@@ -8,14 +8,11 @@ use uucore::error::UResult;
 use crate::errors::DomainNameError;
 use crate::net::domain_name;
 
-
-pub(crate) fn print_domain_name (out: &mut dyn std::io::Write) -> UResult<()> {
+pub(crate) fn print_domain_name(out: &mut dyn std::io::Write) -> UResult<()> {
     if let Some(domain_name) = domain_name()? {
         out.write_all(domain_name.as_bytes())?;
         out.write_all(b"\n").map_err(From::from)
     } else {
         Err(Box::new(DomainNameError::NoLocalDomainName))
-    }   
+    }
 }
-
-
