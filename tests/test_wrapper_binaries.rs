b860fea18d582516d715c29f980fc40aa3756f4a
haitai <hkng248@gmail.com>
2025-12-16 08:21:32 -0500

    run cargo fmt; update ci.yml

--- tests/test_wrapper_binaries.rs
+++ tests/test_wrapper_binaries.rs
@@ -14,7 +14,8 @@ fn run_output_from_cmd(cmd: &mut AssertCommand) -> (i32, String, String) {
 
 fn normalize_output(output: &(i32, String, String)) -> (i32, String, String) {
     use regex::Regex;
-    let re = Regex::new(r"/.*/target/debug/[^\s]+").unwrap_or_else(|_| Regex::new(r"invalid").unwrap());
+    let re =
+        Regex::new(r"/.*/target/debug/[^\s]+").unwrap_or_else(|_| Regex::new(r"invalid").unwrap());
     let stdout = re.replace_all(&output.1, "hostname").to_string();
     let stderr = re.replace_all(&output.2, "hostname").to_string();
     (output.0, stdout, stderr)
@@ -22,29 +23,53 @@ fn normalize_output(output: &(i32, String, String)) -> (i32, String, String) {
 
 #[test]
 fn dnsdomainname_matches_hostname_d() {
-    let a = normalize_output(&run_output_from_cmd(&mut assert_cmd::cargo::cargo_bin_cmd!("dnsdomainname").arg("--help")));
-    let b = normalize_output(&run_output_from_cmd(&mut assert_cmd::cargo::cargo_bin_cmd!("hostname").arg("-d").arg("--help")));
+    let a = normalize_output(&run_output_from_cmd(
+        &mut assert_cmd::cargo::cargo_bin_cmd!("dnsdomainname").arg("--help"),
+    ));
+    let b = normalize_output(&run_output_from_cmd(
+        &mut assert_cmd::cargo::cargo_bin_cmd!("hostname")
+            .arg("-d")
+            .arg("--help"),
+    ));
     assert_eq!(a, b);
 }
 
 #[test]
 fn domainname_matches_hostname_y() {
-    let a = normalize_output(&run_output_from_cmd(&mut assert_cmd::cargo::cargo_bin_cmd!("domainname").arg("--help")));
-    let b = normalize_output(&run_output_from_cmd(&mut assert_cmd::cargo::cargo_bin_cmd!("hostname").arg("-y").arg("--help")));
+    let a = normalize_output(&run_output_from_cmd(
+        &mut assert_cmd::cargo::cargo_bin_cmd!("domainname").arg("--help"),
+    ));
+    let b = normalize_output(&run_output_from_cmd(
+        &mut assert_cmd::cargo::cargo_bin_cmd!("hostname")
+            .arg("-y")
+            .arg("--help"),
+    ));
     assert_eq!(a, b);
 }
 
 #[test]
 fn nisdomainname_matches_hostname_y() {
-    let a = normalize_output(&run_output_from_cmd(&mut assert_cmd::cargo::cargo_bin_cmd!("nisdomainname").arg("--help")));
-    let b = normalize_output(&run_output_from_cmd(&mut assert_cmd::cargo::cargo_bin_cmd!("hostname").arg("-y").arg("--help")));
+    let a = normalize_output(&run_output_from_cmd(
+        &mut assert_cmd::cargo::cargo_bin_cmd!("nisdomainname").arg("--help"),
+    ));
+    let b = normalize_output(&run_output_from_cmd(
+        &mut assert_cmd::cargo::cargo_bin_cmd!("hostname")
+            .arg("-y")
+            .arg("--help"),
+    ));
     assert_eq!(a, b);
 }
 
 #[test]
 fn ypdomainname_matches_hostname_y() {
-    let a = normalize_output(&run_output_from_cmd(&mut assert_cmd::cargo::cargo_bin_cmd!("ypdomainname").arg("--help")));
-    let b = normalize_output(&run_output_from_cmd(&mut assert_cmd::cargo::cargo_bin_cmd!("hostname").arg("-y").arg("--help")));
+    let a = normalize_output(&run_output_from_cmd(
+        &mut assert_cmd::cargo::cargo_bin_cmd!("ypdomainname").arg("--help"),
+    ));
+    let b = normalize_output(&run_output_from_cmd(
+        &mut assert_cmd::cargo::cargo_bin_cmd!("hostname")
+            .arg("-y")
+            .arg("--help"),
+    ));
     assert_eq!(a, b);
 }
 
@@ -53,5 +78,3 @@ fn ping_wrapper_tests() {
     // sanity test to ensure this test file is discovered by the harness
     assert!(true);
 }
-
-
